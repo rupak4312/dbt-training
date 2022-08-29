c04d16b47946e62eb9afd06204ba630eda9345a0
rupak_pal@persistent.com <rupak_pal@persistent.com>
2022-08-29 10:20:04 +0000

    created report, stg, raw everything

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -6,14 +6,16 @@ o.shipmode,
 o.ordersellingprice - o.ordercostprice as orderprofit,
 o.ordersellingprice,
 o.ordercostprice,
+c.customerid,
 c.customername,
 c.segment,
 c.country,
+p.productid,
 p.category,
 p.productname,
 p.subcategory
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
-on o.productid = c.customerid
+on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid = p.productid
